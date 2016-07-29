f6bb8540f7d5c95a54c384bee83683543ea12651
ManiacViper <kenneth.d.monteiro@gmail.com>
2016-07-29 20:28:13 +0100

    added test for max page count

--- src/main/navigator.rb
+++ src/main/navigator.rb
@@ -4,13 +4,20 @@ require 'open-uri'
 
 module NAVIGATOR
 
-  WEBSITE = 'http://www.wegotickets.com/searchresults/all'
+  FIRST_WEBPAGE = 'http://www.wegottickets.com/searchresults/adv'
+  PAGINATE_WEB_LINK = 'http://www.wegottickets.com/searchresults/page/2/adv#paginate'
 
   def initialize()
   end
 
-  def goToWebsite(website)
-    page = Nokogiri::HTML(open(website))
+  def getPageCount(page)
+    allPaginationLinks = page.css("a[class='pagination_link']")
+    pageCount = allPaginationLinks[allPaginationLinks.length]
+    pageCount
+  end
+
+  def goToWebpage(webpage)
+    page = Nokogiri::HTML(open(webpage))
     page
   end
 end
--- src/test/navigator-spec.rb
+++ src/test/navigator-spec.rb
@@ -6,8 +6,14 @@ include NAVIGATOR
 
 describe 'My behaviour' do
 
-  it 'should do something' do
-    page = NAVIGATOR.goToWebsite(NAVIGATOR::WEBSITE)
-    expect(page).not_to be_nil
+  it 'should go to web page' do
+    page = NAVIGATOR.goToWebpage(NAVIGATOR::FIRST_WEBPAGE)
+    expect(page.css("title").text).to eq('WeGotTickets | Simple, honest ticketing')
+  end
+
+  it 'should get the last page number in the pagination search results' do
+    page = NAVIGATOR.goToWebpage(NAVIGATOR::FIRST_WEBPAGE)
+    pageCount = NAVIGATOR.getPageCount(page)
+    expect(pageCount).not_to eq(736) # not the best test as the max page can change
   end
 end
